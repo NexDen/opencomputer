a0a910aced4bfafa6d4a0a94e09e365fc10411db
Yiğit Emin KOLAN <74785479+NexDen@users.noreply.github.com>
2024-11-26 01:15:31 +0300

    Update opencomputer.lua

--- opencomputer.lua
+++ opencomputer.lua
@@ -3,7 +3,7 @@ r.activate()
 maxHeat = r.getMaxHeatLevel()
 while true do
   local heat = r.getHeatLevel()
-  if heat > 100000 then
+  if heat > 2400000 then
     print("SCRAM! SCRAM! SCRAM!")
     r.deactivate()
     break
