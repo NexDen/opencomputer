01dab65b172d2e3f312ed8f71c1c016ad53f632e
Yiğit Emin KOLAN <74785479+NexDen@users.noreply.github.com>
2024-11-26 01:12:28 +0300

    Add files via upload

--- opencomputer.lua
+++ opencomputer.lua
@@ -1,11 +1,12 @@
 r = component.proxy(component.list("nc_fission_reactor")())
-
+r.activate()
 maxHeat = r.getMaxHeatLevel()
 while true do
   local heat = r.getHeatLevel()
   if heat > 1000 then
     print("SCRAM! SCRAM! SCRAM!")
     r.deactivate()
+    break
   end
   os.sleep(1)
 end
