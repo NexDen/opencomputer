fd2a793d4b6883a07b2bb797f43af0973dcb0dec
NexDen <thenexden@gmail.com>
2024-11-26 20:14:03 +0300

    update

--- opencomputer.lua
+++ opencomputer.lua
@@ -1,13 +1,21 @@
 r = component.proxy(component.list("nc_fission_reactor")())
 r.activate()
 maxHeat = r.getMaxHeatLevel()
+is_overheated = false
 while true do
   local heat = r.getHeatLevel()
-  print(heat)
-  if heat > 2400000 then
+  if heat > (maxHeat - 100000) then
     print("SCRAM! SCRAM! SCRAM!")
     r.deactivate()
+    is_overheated = true
     break
+  end
+  if is_overheated then
+    if heat < 100000 then
+      print("Reactivating reactor")
+      r.activate()
+      is_overheated = false
+    end
   end
   os.sleep(1)
 end
